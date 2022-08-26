9e6621aa1f268d4254e92895a82c1ffd60d6a822
Stian Kodehode <stiant.kodehode@gmail.com>
2022-08-26 10:13:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { FlexRow, StyledParagraph } from "./components/styled";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [catFacts, setCatFacts] = useState();
+
+    useEffect(() => {
+        axios
+            .get("https://catfact.ninja/facts")
+            .then((res) => {
+                setCatFacts(res.data.data);
+            })
+            .catch(console.log("Error"));
+    }, []);
+
+    if (catFacts) {
+        return (
+            <div className="App">
+                <FlexRow>
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1>React Tasks (API Call)</h1>
+                    <img src={logo} className="App-logo" alt="logo" />
+                </FlexRow>
+                {/* Mapping the catfacts to elements */}
+                {catFacts.map((ele, idx) => {
+                    return (
+                        <StyledParagraph key={idx}>{ele.fact}</StyledParagraph>
+                    );
+                })}
+            </div>
+        );
+    } else {
+        return (
+            <div className="App">
+                <h1>Loading...................</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const FlexRow = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1em;
+`;
+
+export const StyledParagraph = styled.p`
+    color: cyan;
+`;
